943e925f23ca022bb533180cbe290d7311afc9a7
hayato0309 <hayato.pluto.4538@gmail.com>
2021-11-27 19:29:15 +0900

    5-2-5 ¤数 p520

---
+++ download2/images.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1); ?>
+
+<body>
+    <p>丸マークの画像</p>
+    <img src="show-image.php?type=circle">
+
+    <p>三角マークの画像</p>
+    <img src="show-image?type=triangle">
+</body>
